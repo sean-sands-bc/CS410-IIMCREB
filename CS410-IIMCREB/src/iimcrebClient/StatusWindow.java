2d62246555a9729bd5d14875871dfd753c5a41e1
sean-sands-bc <45183645+sean-sands-bc@users.noreply.github.com>
2019-05-28 19:18:20 -0700

    Add files via upload

--- CS410-IIMCREB/src/iimcrebClient/StatusWindow.java
+++ CS410-IIMCREB/src/iimcrebClient/StatusWindow.java
@@ -12,5 +12,31 @@ public class StatusWindow extends JFrame {
 		setTitle("Status");
 		
 		JPanel staPnl = new JPanel();
+		staPnl.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		
+		JButton staOnBtn = new JButton("Online");
+		JButton staBuBtn = new JButton("Busy");
+		JButton staOfBtn = new JButton("Offline");
+		
+		c.gridx = 0;
+		c.gridy = 0;
+		c.weighty = 0.5;
+		c.weightx = 0.5;
+		
+		staPnl.add(staOnBtn, c);
+		
+		c.gridy = 1;
+		
+		staPnl.add(staBuBtn, c);
+		
+		c.gridy = 2;
+		
+		staPnl.add(staOfBtn, c);
+		
+		add(staPnl);	//	add panel to frame
+		setPreferredSize(new Dimension(100, 200));	//	preferred frame size
+		pack();	//	pack frame
+		setVisible(true);	//	show frame
 	}
 }
